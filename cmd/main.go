a19b41fdcebda27dd42819e57e975254db7512f8
SergeyShakhbazyan <sergey.shakhbazyan16@gmail.com>
2024-12-27 16:29:40 +0400

    Удалены postgresDB и config/db.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"ProjectDB/config"
-	"fmt"
-	"log"
-)
+import "ProjectDB/internal/app"
 
 func main() {
-	conn := config.NewConnection()
-	defer conn.Close()
-
-	var version string
-	err := conn.DB.QueryRow("SELECT version();").Scan(&version)
-	if err != nil {
-		log.Fatalf("Error querying database: %v", err)
-	}
-
-	fmt.Printf("PostgreSQL Version: %s\n", version)
+	app.StartApp()
 }
