b95a855a08d9e5c93920423fb5f4539610566638
SergeyShakhbazyan <sergey.shakhbazyan16@gmail.com>
2024-12-27 19:21:04 +0400

    Added database initialization files (docker-compose.yml, schema.sql)

--- internal/postgresDB/schema.sql
+++ internal/postgresDB/schema.sql
@@ -1,1 +1,27 @@
+CREATE TABLE equipment(
+    inventory_number SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    manufacturer VARCHAR(255) NOT NULL,
+    start_date DATE,
+    lifeTime INTEGER
+);
 
+CREATE TABLE material (
+    material_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255),
+    unit_price NUMERIC(10, 2),
+    unit_of_measurement VARCHAR(50),
+    alternative VARCHAR(255)
+);
+
+CREATE TABLE product_specification (
+    specification_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    production_duration INTEGER,
+    equipment_id INTEGER NOT NULL,
+    material_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY (equipment_id) REFERENCES equipment (inventory_number) ON DELETE CASCADE,
+    FOREIGN KEY (material_id) REFERENCES material (material_id) ON DELETE CASCADE
+);
